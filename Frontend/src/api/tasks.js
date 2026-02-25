f364404334983bfd97a0a757ea42bf55f8125b8e
22258108@life.hkbu.edu.hk <13716698025@163.com>
2026-02-25 15:27:53 +0800

    minor fixes

--- Frontend/src/api/tasks.js
+++ Frontend/src/api/tasks.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000'
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'
 console.log('API_BASE:', API_BASE)
 console.log('All env:', import.meta.env)
 
